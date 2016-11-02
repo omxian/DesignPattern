64cc53aee8e2d8b4feb45ad5609fe1aa57445222
omxian <104002782@qq.com>
2016-11-02 22:56:12 +0800

    工厂方法模式

---
+++ DesignPattern/AbstractFactoryPattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class AbstractFactoryPattern
+    {
+        /*
+            Abstract Factory Pattern(抽象工厂方法)
+            提供一个接口，用于创建相关或依赖对象的家族，
+            而不需要明确指定具体类。
+         */
+        public AbstractFactoryPattern()
+        {
+        }
+    }
+
+}
---
+++ DesignPattern/FactoryMethodPattern.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class FactoryMethodPattern
+    {
+        /*
+            Factory Method Pattern(工厂方法模式)
+            定义了一个创建对象的接口，但由子类决定要实例化的类是哪一个。
+            工厂方法把实例化推迟到子类。
+
+            如果由新的一套实例化规则，可以简单的新建一个新的工厂。
+            面向修改封闭，面向扩展开放。
+         */
+
+        public FactoryMethodPattern()
+        {
+            ///*测试代码
+            //由子类决定要实例化的是什么类
+            new FactoryA().Produce(ProductType.A);
+            new FactoryB().Produce(ProductType.A);
+            /**/
+        }
+    }
+
+    /// <summary>
+    /// 每个子类的具体是实现可以完全不同
+    /// </summary>
+    public class FactoryB : IFactory
+    {
+        public IProduct Produce(ProductType type)
+        {
+            if (type == ProductType.A)
+            {
+                return new ProductA();
+            }
+            else
+            {
+                return new ProductD();
+            }
+        }
+    }
+
+    public class FactoryA : IFactory
+    {
+        public IProduct Produce(ProductType type)
+        {
+            if (type == ProductType.A)
+            {
+                return new ProductA();
+            }
+            else 
+            {
+                return new ProductB();
+            }
+        }
+    }
+    public class ProductA : IProduct
+    {
+    }
+    public class ProductB : IProduct
+    {
+    }
+    public class ProductC : IProduct
+    {
+    }
+    public class ProductD : IProduct
+    {
+    }
+
+    /// <summary>
+    /// 工厂的基类，暴露在外的接口
+    /// </summary>
+    public interface IFactory
+    {
+        //子类可以自己决定怎么去实例化
+        IProduct Produce(ProductType productType);
+    }
+
+    /// <summary>
+    /// 工厂实例化的基类
+    /// </summary>
+    public interface IProduct
+    {
+    }
+
+    public enum ProductType
+    {
+        A,
+        B,
+        C,
+        D
+    }
+}
