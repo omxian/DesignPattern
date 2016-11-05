c32cad0a5ce2269d342725a5f5ebf77d3daa4283
omxian <104002782@qq.com>
2016-11-05 23:38:55 +0800

    外观模式，适配者模式，添加钩子hook示例

--- DesignPattern/TemplateMethodPattern.cs
+++ DesignPattern/TemplateMethodPattern.cs
@@ -10,7 +10,9 @@ namespace DesignPattern.TemplateMethod
                 Template Method Pattern(模板方法模式)
                 在一个方法中定义一个算法的骨架，而将一些步骤延迟到子类中。
                 模板方法使得子类可以在不改变算法结构的情况下，重新定义算法中的某些步骤。
-                
+            
+                还可以使用钩子，让模板方法模式更灵活。    
+
                 其实Unity中的Start,Update等方法就是使用了模板方法模式，
                 C#中的IComparable也是很好的例子。
              */
@@ -26,6 +28,16 @@ namespace DesignPattern.TemplateMethod
             {
                 Console.WriteLine("Brew Coffee");
             }
+
+            public override bool IsCustom()
+            {
+                return true;
+            }
+
+            public override void CustomHook()
+            {
+                Console.WriteLine("Custom void Hook");
+            }
         }
 
         public abstract class MakeDrink
@@ -35,6 +47,14 @@ namespace DesignPattern.TemplateMethod
                 BoilWater();
                 Brew();
                 PourInCup();
+                //有两种钩子方法
+                //这种做法称为挂钩
+                if (IsCustom())
+                {
+                    Custom();
+                }
+                //这种需要实现方法默认为空
+                CustomHook();
             }
 
             public void BoilWater()
@@ -43,6 +63,20 @@ namespace DesignPattern.TemplateMethod
             }
             public abstract void Brew();
             
+            public virtual bool IsCustom()
+            {
+                return false;
+            }
+            public void Custom()
+            {
+                Console.WriteLine("Custom");
+            }
+
+            public virtual void CustomHook()
+            {
+
+            }
+
             public void PourInCup()
             {
                 Console.WriteLine("PourInCup");
